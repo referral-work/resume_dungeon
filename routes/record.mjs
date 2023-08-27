80ee32bb63b47f28704b4d23192b614ed201da2a
referral-work <referral.work.22@gmail.com>
2023-08-27 22:09:37 +0530

    feature: iratings

--- routes/record.mjs
+++ routes/record.mjs
@@ -3,16 +3,20 @@ import db from "../db/conn.mjs";
 
 const router = express.Router();
 
-// This section will help you get a list of all iusers
 router.get("/iusers", async (req, res) => {
   let collection = await db.collection("iusers");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
 
-// This section will help you get a single record by id
 router.get("/ilogs", async (req, res) => {
   let collection = await db.collection("ilogs");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+
+router.get("/iratings", async (req, res) => {
+  let collection = await db.collection("iratings");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
